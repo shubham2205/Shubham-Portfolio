05bce72a1e51497e7e071f841152d197056aa7c8
Shubham Dharpure <shubhamdharpure01@gmail.com>
2023-10-31 10:41:15 +0530

    .s

--- src/components/Whatsapp/WhatsappChat.js
+++ src/components/Whatsapp/WhatsappChat.js
@@ -5,7 +5,7 @@ const WhatsappChat = () => {
   return (
     <>
       <div className="main">
-        <a  href=" https://wa.me/919981965973?text=Thanks For Connecting With Me" target="_blank">
+        <a  href=" https://wa.me/919981965973?text=Thanks For Connecting With Me....😊" target="_blank">
           <img className="whatsapp_logo" src="https://static.vecteezy.com/system/resources/previews/022/101/124/original/whatsapp-logo-transparent-free-png.png" alt="whatsapp-logo" />
         </a>
       </div>
